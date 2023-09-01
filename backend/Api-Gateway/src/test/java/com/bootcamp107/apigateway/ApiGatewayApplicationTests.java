00bddaa975efbc30819b90d0d09c760b40a95346
Ravi Kant <127819443+ravikant-216@users.noreply.github.com>
2023-09-01 08:25:15 +0530

    feat: Backend Scaffolding (#79)

---
+++ backend/Api-Gateway/src/test/java/com/bootcamp107/apigateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,11 @@
+package com.bootcamp107.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
---
+++ backend/Business-Service/src/test/java/com/bootcamp107/busines/service/BusinessServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bootcamp107.busines.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusinessServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

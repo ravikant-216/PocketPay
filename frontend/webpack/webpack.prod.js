1d9fba7fcbeab93c04778bf8fa334220e63abd8f
Sri Charan Yella <sricharan.yella@zemosolabs.com>
2023-09-04 16:45:20 +0530

    added build script

---
+++ frontend/webpack/webpack.prod.js
@@ -0,0 +1,53 @@
+const path = require('path')
+const HTMLWebpackPlugin = require('html-webpack-plugin')
+const Dotenv = require('dotenv-webpack')
+
+module.exports = {
+  entry: path.resolve(__dirname, '..', './src/index.tsx'),
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|js)x?$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'babel-loader',
+          },
+        ],
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(woff(2)?|eot|ttf|otf|svg|)$/,
+        type: 'asset/inline',
+      },
+    ],
+  },
+  output: {
+    path: path.resolve(__dirname, '..', './build'),
+    filename: 'bundle.js',
+  },
+  mode: 'production',
+  devtool: 'source-map',
+  plugins: [
+    new HTMLWebpackPlugin({
+      template: path.resolve(__dirname, '..', './public/index.html'),
+    }),
+    new Dotenv(),
+  ],
+  devServer: {
+    host: 'localhost',
+    port: 3001,
+    historyApiFallback: true,
+    open: true,
+  },
+}
